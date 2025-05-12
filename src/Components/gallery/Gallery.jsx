5ed7db35ffdd96486b06fc17245cba6ab440b2a0
silke-J <silk0776@mcdmstudent.dk>
2025-05-12 11:09:46 +0200

    base

---
+++ src/Components/gallery/Gallery.jsx
@@ -0,0 +1,159 @@
+import styles from "./Gallery.module.css";
+import it24 from "../../../public/gallery/24.jpg";
+import it23 from "../../../public/gallery/23.jpg";
+import it22 from "../../../public/gallery/22.jpg";
+import it21 from "../../../public/gallery/21.jpg";
+import it20 from "../../../public/gallery/20.jpg";
+import it19 from "../../../public/gallery/19.jpg";
+import it18 from "../../../public/gallery/18.jpg";
+import it17 from "../../../public/gallery/17.jpg";
+import it16 from "../../../public/gallery/16.jpg";
+import it15 from "../../../public/gallery/15.jpg";
+import it14 from "../../../public/gallery/14.jpg";
+import it13 from "../../../public/gallery/13.jpg";
+import it12 from "../../../public/gallery/12.jpg";
+import it11 from "../../../public/gallery/11.jpg";
+import it10 from "../../../public/gallery/10.jpg";
+import it9 from "../../../public/gallery/9.jpg";
+import it8 from "../../../public/gallery/8.jpg";
+import it7 from "../../../public/gallery/7.jpg";
+import it6 from "../../../public/gallery/6.jpg";
+import it5 from "../../../public/gallery/5.jpg";
+import it4 from "../../../public/gallery/4.jpg";
+import it3 from "../../../public/gallery/3.jpg";
+import it2 from "../../../public/gallery/2.jpg";
+import it1 from "../../../public/gallery/1.jpg";
+
+
+const Galleryitalien = () => {
+  return (
+    <>
+      <section className={styles.pics}>
+        <div className={styles.pic}>
+          <img src={it20} alt="" />
+          <p>Italien</p>
+          <p>2024</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it21} alt="" />
+          <p>Italien</p>
+          <p>2024</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it22} alt="" />
+          <p>Italien</p>
+          <p>2024</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it23} alt="" />
+          <p>Italien</p>
+          <p>2024</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it24} alt="" />
+          <p>Italien</p>
+          <p>2024</p>
+        </div>
+
+        <div className={styles.pic}>
+          <img src={it11} alt="" />
+          <p>Italien</p>
+          <p>2023</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it12} alt="" />
+          <p>Italien</p>
+          <p>2023</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it13} alt="" />
+          <p>Italien</p>
+          <p>2023</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it14} alt="" />
+          <p>Italien</p>
+          <p>2023</p>
+        </div>
+        <div className={styles.piC}>
+          <img src={it15} alt="" />
+          <p>Italien</p>
+          <p>2023</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it16} alt="" />
+          <p>Italien</p>
+          <p>2023</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it17} alt="" />
+          <p>Italien</p>
+          <p>2023</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it18} alt="" />
+          <p>Italien</p>
+          <p>2023</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it19} alt="" />
+          <p>Italien</p>
+          <p>2023</p>
+        </div>
+
+        <div className={styles.pic}>
+          <img src={it1} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it2} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it3} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it4} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it5} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it6} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it7} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it8} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it9} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+        <div className={styles.pic}>
+          <img src={it10} alt="" />
+          <p>Italien</p>
+          <p>2022</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Galleryitalien;
